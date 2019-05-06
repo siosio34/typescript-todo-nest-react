c27cec0d87895a8ac73a6fcdd468303882202567
youngje jp <siosio3103@gmail.com>
2019-05-06 14:39:38 +0000

    Apply styled-components

--- client/src/pages/todo/todo.tsx
+++ client/src/pages/todo/todo.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const TodoWrapper = styled.div`
+    padding: 16px;
+    background: #f2f2f2;
+    display: flex;
+    height: 100%;
+    `
+const TodoTitle = styled.h3`
+    font-size: 1.5em;
+    text-align: center;
+    `
 class TodoPage extends React.Component {
     
     render(){
         return (
-            <div>TODO PAGE</div>
+        <>
+            <TodoWrapper>
+                <TodoTitle>TODO</TodoTitle>
+            </TodoWrapper>
+        </>
             )
     }
 }
