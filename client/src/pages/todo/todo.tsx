e12dd61cda864d76923a2b9556f87e71a243770f
youngje jp <siosio3103@gmail.com>
2019-05-08 11:18:29 +0000

    Add simple todo (add)

--- client/src/pages/todo/todo.tsx
+++ client/src/pages/todo/todo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from 'antd';
+import { Button, List, Input } from 'antd';
 
 const TodoWrapper = styled.div`
     background: #f2f2f2;
@@ -15,26 +15,64 @@ const TodoCard = styled.div`
     flex-direction: column;
     display: flex;
     align-items: center;
-    width: 50em;
-    height: 600px;
+    min-width: 400px;
+    min-height: 600px;
     background: #ffffff;
 `
 const TodoTitle = styled.h3`
     font-size: 1.5em;
-    text-align: center;
-    `
-class TodoPage extends React.Component {
+    text-align: center;`
+    
+type TodoProps = {
+    
+}
+
+type TodoState = {
+    currentInput: string;
+    todos: { text: string, done: boolean }[];
+}
+
+class TodoPage extends React.Component<TodoProps, TodoState> {
+    
+    state: TodoState = {
+        currentInput: '',
+        todos: [],
+    }
+    
+    onChangeTodoInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({
+            currentInput: e.target.value
+        })
+    }
+    
+    onClickAddTodoBtn = (): void => {
+        const newTodos = this.state.todos.concat({
+           text: this.state.currentInput,
+           done: false,
+        });
+        
+        this.setState({
+            currentInput: '',
+            todos: newTodos
+        })
+        
+    }
     
     render(){
         return (
-        <>
             <TodoWrapper>
                 <TodoCard>
                     <TodoTitle>TODO</TodoTitle>
-                    <Button shape="circle" type="primary" icon="plus" />
+                    <Input onChange={this.onChangeTodoInput} style={{ marginBottom: 8}} placeholder="오늘 뭐하지? "/>
+                    <Button style={{ marginBottom: 16}} shape="circle" type="primary" icon="plus" onClick={this.onClickAddTodoBtn}/>
+                    <List
+                        size="small"
+                        bordered
+                        dataSource={this.state.todos}
+                        renderItem={item => (<List.Item>{item.text}</List.Item>)}
+                        />
                 </TodoCard>
             </TodoWrapper>
-        </>
             )
     }
 }
